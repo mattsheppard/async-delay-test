ba3b0a66507b66356e32038d35c3bec2b7203c06
Matthew Sheppard <msheppard@funnelback.com>
2019-02-11 09:48:49 +1100

    Improved reporting a little
    We see a number of valid responses which are instantly returned
    but I don't at this stage know why. It seems like any delay that's occurring
    might be to do with the web server rather than our explicit delay because
    we see more of them if randomise the request starts a little.

--- src/test/java/TestServer.java
+++ src/test/java/TestServer.java
@@ -6,6 +6,7 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -26,30 +27,39 @@ public class TestServer {
         // pending request each
 
         for (int concurrentThreads = 1; concurrentThreads < 11; concurrentThreads++) {
-            final AtomicInteger counter = new AtomicInteger(0),
-                    totalDelayedRequest = new AtomicInteger(0),
-                    totalInstantRequests = new AtomicInteger(0);
+            final AtomicInteger
+                validDelayedResponses = new AtomicInteger(0),
+                validInstantResponses = new AtomicInteger(0),
+                invalidDelayedResponses = new AtomicInteger(0),
+                invalidInstantResponses = new AtomicInteger(0);
 
             ExecutorService executor = Executors.newFixedThreadPool(concurrentThreads);
 
             for (int i = 0; i < 300; i++) {
                 executor.submit(() -> {
                     try {
+                        // Try to spread the requests out a little
+                        // Oddly if I enable this I see many more valid-instant responses.
+                        //
+                        //Thread.sleep(ThreadLocalRandom.current().nextInt(0, 50));
+
                         long startTime = System.currentTimeMillis();
                         String result = new String(new URL("http://127.0.0.1:8080/delayed-hello").openStream().readAllBytes(),
                                 StandardCharsets.UTF_8);
                         long respTime = System.currentTimeMillis() - startTime;
 
-                        if (respTime < 1000) {
-                            System.err.printf("INSTANT: Got an instant response [%s] after %dms\n", result, respTime);
-                            totalInstantRequests.incrementAndGet();
-                        } else if (!result.equals("Hello, world!")) {
-                            System.err.printf("DELAYED : Unexpectedly got [%s]. Delay: %dms\n", result, respTime);
-                            totalDelayedRequest.incrementAndGet();
+                        if (respTime < 1000 && result.equals("Hello, world!\n")) {
+                            validInstantResponses.incrementAndGet();
+                        } else if (respTime >= 1000 && result.equals("Hello, world!\n")) {
+                            validDelayedResponses.incrementAndGet();
+                        } else if (respTime < 1000) {
+                            // Could warn about the invalid value of result here
+                            invalidInstantResponses.incrementAndGet();
                         } else {
-                            counter.incrementAndGet();
+                            // Could warn about the invalid value of result here
+                            invalidDelayedResponses.incrementAndGet();
                         }
-                    } catch (IOException e) {
+                    } catch (IOException | InterruptedException e) {
                         throw new RuntimeException(e);
                     }
                 });
@@ -57,9 +67,17 @@ public class TestServer {
 
             executor.awaitTermination(10, TimeUnit.SECONDS);
 
-            System.out.println("Client Threads: " + concurrentThreads + " - Got " + counter.get() + " responses in 10 secs.");
-            System.out.printf("Total delayed requests %d served\n", totalDelayedRequest.get());
-            System.out.printf("Total instant requests %d served\n", totalInstantRequests.get());
+            System.out.printf("Client Threads: %d \n"
+                + "\tvalid-instant: %d \n"
+                + "\tvalid-delayed: %d \n"
+                + "\tinvalid-instant: %d\n"
+                + "\tinvalid-delayed: %d\n"
+                + "\n", concurrentThreads,
+                validInstantResponses.get(), validDelayedResponses.get(),
+                invalidInstantResponses.get(), invalidDelayedResponses.get());
+
+            // Give the server a chance to clear any pending requests we aborted.
+            Thread.sleep(2000);
         }
         server.stop();
     }
